4ef6d1a4909a918fb75c831ddcc0023ce061afb5
Badar Ur Zaman <hbrsadiqian123@gmail.com>
2023-12-15 22:30:41 +0500

    onlineApiFetch

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,13 +1,22 @@
-import axios from 'axios';
+import axios from "axios";
 
-const BASE_URL = 'http://localhost:8000/api';
+const BASE_URL = "https://api.themoviedb.org/3";
 
-export const fetchDataFromApi = async (url, params) => {
-  try {
-    const { data } = await axios.get(`${BASE_URL}${url}`, { params });
-    return data;
-  } catch (error) {
-    console.error(error);
-    return error;
-  }
+const TMDB_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5Y2ZmN2NhYTg0YzZjYzE4NGRiMTBkODY3OGZiMjg5YSIsInN1YiI6IjY1Njg4NDgzZmI1Mjk5MDExZjcwZDc5ZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.a7XZ755V0EfThS5HSV4xTKAeXDxsa0ii2FJ1QSCUIng";
+
+const header = {
+    Authorization: "bearer " + TMDB_TOKEN,
 };
+
+export const fetchDataFromApi = async(url, params)=>{
+    try {
+        const {data} = await axios.get(
+            BASE_URL + url,{
+                headers: header, params
+            })
+        return data;
+    } catch (error) {
+        console.log(error);
+        return error;
+    }
+}
